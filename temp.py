8fc6cf390576d29442811a72de547a6e35c0db7b
Daniel McMillan <mcmillan@hotmail.com.au>
2016-02-04 23:40:17 +1100

    Add automatic updating of temperature data

--- temp.py
+++ temp.py
@@ -2,47 +2,77 @@
 
 import json
 import argparse
-import sensor_reader
+import time
+from sensor_reader import SensorReader
+from rrd_connection import RrdConnection
 
 default_config = "config.json"
+#todo: add more default values
 
 #Parse the command line arguments
 parser = argparse.ArgumentParser(description="Read from temperature sensors.")
-parser.add_argument("-c", "--config", help="Specify the config file (default: " + default_config + ")", default=default_config)
-rrdarg = parser.add_argument_group(title="Round Robin Database", description="Options for providing data to an rrdtool server")
-#rrdarg.add_argument("-d", "--database", help="Connect to the configured rrdtool server and add temperature data to the specified rrd file")
-#rrdarg.add_argument("-n", "--newrrd", help="Specifies that a new rrd file should be created instead of updating an existing database",
-#    action="store_true")
+parser.add_argument("-c", "--config", help="Specify the config file (default: " + default_config + ")",
+    default=default_config)
+
+rrdarg = parser.add_argument_group(title="Round Robin Database",
+    description="Options for providing data to an rrdtool server")
+rrdarg.add_argument("-d", "--database", help= "Connect to the configured rrd and provide temperature " +
+    "data with the configured interval", action="store_true")
+rrdarg.add_argument("-n", "--newrrd", help=
+    "Connect to rrdtool and create the database, clearing existing data", action="store_true")
 args = parser.parse_args()
 
 #Try to load the config
 try:
     config = json.load(open(args.config))
-    if not "device_path" in config:
-        config["device_path"] = default_device
-except (IOError, ValueError):
+except (IOError, ValueError) as ex:
     print("Failed to load config file \"" + args.config + "\"")
+    print("Stack trace: " + ex)
     quit()
     
+if not "device_path" in config:
+    config["device_path"] = default_device
+    
 sensors = config["sensors"]
-reader = sensor_reader.SensorReader(config["device_path"], sensors)
+reader = SensorReader(config["device_path"], sensors)
 
-if (args.database == None):
-    if (args.newrrd):
-        print("Cannot create rrd file without connecting to rrdtool")
-    else:
-        #Print all of the sensor values
-        temps = reader.readAllTemps()
-        for sensor_id in temps:
-            sensor = sensors[sensor_id]
-            temp = temps[sensor_id]
-            if temp == temp:
-                print(sensor["name"] + ": " + str(temp) + u"\N{DEGREE SIGN}C")
-            else:
-                #temp is NaN
-                print(sensor["name"] + ": Unknown")
+if not (args.database or args.newrrd):
+    #Print all of the sensor values
+    temps = reader.readAllTemps()
+    for sensor_id in temps:
+        sensor = sensors[sensor_id]
+        temp = temps[sensor_id]
+        if temp == temp:
+            print(sensor["name"] + ": " + str(temp) + "\N{DEGREE SIGN}C")
+        else:
+            #temp is NaN
+            print(sensor["name"] + ": Unknown")
+            print
 else:
-    print("Connect to rrdtool server")
-    #Tell it to connect, get an object for the connection
-    #Tell it to create a new database if required
-    #Tell it to start updating
+    print("Starting connection to RRDTOOl server")
+    with RrdConnection(config["rrd_address"], config["rrd_port"], config["database_name"],
+            sensors, config["update_delay"]) as rrd:
+        if (args.newrrd):
+            #Create the database
+            ans = input("Are you sure you want to create database {0}? ".format(config["database_name"]) +
+                "Existing data will be permanently removed. (y/n) ")
+            if ans.lower() == "y":
+                rrd.create()
+        else:
+            #Start performing updates
+            delay = int(config["update_delay"])
+            nextupdate = time.time()
+            
+            while True:
+                rrd.update(reader.readAllTemps())
+                nextupdate += delay
+                
+                #Sleep until the next delay second interval
+                sleeptime = nextupdate - time.time()
+                #Dont sleep and reset if dropping behind
+                if (sleeptime < 0):
+                    nextupdate = time.time()
+                else
+                    time.sleep(sleeptime)
+                
+            
