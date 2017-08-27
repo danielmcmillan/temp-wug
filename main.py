fae71a62662b12b4d5817f5e91d0bce465fd920c
Daniel McMillan <mcmillan@hotmail.com.au>
2017-08-27 13:19:10 +1000

    Implement uploading to Wunderground

--- main.py
+++ main.py
@@ -3,13 +3,15 @@
 """Program for reading temperatures from sensor devices and providing them to Weather Underground"""
 import sys
 import argparse
-import socket
 import time
 
 from logger import Logger
+from logger import LogLevel
 from parse_json import parse_json
 from wug_pws import WugPws
 import w1_temp_reader
+
+# TODO: detect erroneous readings
 
 # The default config path
 DEFAULT_CONFIG = "config.json"
@@ -35,18 +37,16 @@ def main():
         return 1
     
     # Create logger
-    log = Logger(config["log_email"])
+    log = Logger(args.verbose, config["log_email"], config["min_email_period"])
 
     # Create WUG PWS for uploading data
-    pws = WugPws(config["weather_underground"], log)
+    pws = WugPws(config["weather_underground"], log, args.noupload)
 
-    read_sensors(config, logger, pws)
-        
-    # args.verbose: log more
-    # args.noupload: stale
+    read_sensors(config, log, pws)
 
 def parse_arguments():
     """ Parse command line arguments.
+
         Return the collection of arguments
     """
     parser = argparse.ArgumentParser(description="Provide sensor data to Weather Underground.")
@@ -66,18 +66,38 @@ def parse_arguments():
     return parser.parse_args()
 
 def read_sensors(config, log, pws):
-    """ Loop while reading sensor values and providing to PWS"""
+    """Loop while reading sensor values and providing to PWS"""
 
     update_delay = int(config["update_delay"])
     upload_delay = int(config["upload_delay"])
-
+    
     # The time when the data was last uploaded
     last_upload = time.time()
 
     while True:
         # Read every configured sensor and provide value to pws
+        for sensor in config["sensors"]:
+            # Get the module required to read the value
+            reader = SENSOR_MODULES.get(sensor["hw_type"])
+            if not reader:
+                log.log("Unable to read sensors with hw_type {}".format(sensor["hw_type"]))
+            # Read the value and log errors
+            value = reader.read_sensor(sensor)
+            if not value:
+                log.log("Error reading {} sensor for {}".format(sensor["hw_type"],
+                    sensor["wug_type"]), LogLevel.CRITICAL)
+                continue
+            log.log("Read value {} from sensor for {}".format(value, sensor["wug_type"]), LogLevel.INFO)
+
+            # Record the value
+            pws.record_value(sensor["wug_type"], value)
+        
         # If required time has passed, upload values
-        pass
+        if last_upload + upload_delay <= time.time():
+            pws.upload_values()
+            last_upload = time.time()
+
+        time.sleep(update_delay)
 
 if __name__ == '__main__':
     sys.exit(int(main() or 0))
--- wug_pws.py
+++ wug_pws.py
@@ -1,19 +1,86 @@
 """Module for uploading sensor data to Weather Underground PWS"""
 
+from datetime import datetime
+from urllib.parse import urlencode
+from urllib.request import urlopen
+from urllib.error import URLError
+from logger import LogLevel
+
 class WugPws:
     """ Accumulates sensor readings and uploads them when requested."""
 
-    def __init__(self, config, log):
-        """Initialise with given configuration."""
+    def __init__(self, config, log, stale=False):
+        """Initialise with given configuration.
+
+            stale: if True, no data will actually be uploaded.
+        """
         self._config = config
         self._log = log
+        self._stale = stale
+
+        # Map wug_type to [total, count]
+        self._data = dict()
     
-    def record_value(self, type, value):
+    def record_value(self, wug_type, value):
         """ Accumulates a value ready to upload.
         
             type: the wug_type for the sensor, determines the field to upload data to.
             value: a floating point value.
         """
+        if wug_type in self._data:
+            self._data[wug_type][0] += value
+            self._data[wug_type][1] += 1
+        else:
+            self._data[wug_type] = [value, 1]
     
     def upload_values(self):
         """Provide accumulated values to Weather Underground"""
+
+        # Compute averages for recorded values
+        fields = dict()
+        for wug_type, values in self._data.items():
+            if values[1] >= self._config["min_readings"]:
+                fields[wug_type] = values[0] / values[1]
+            else:
+                self._log.log("Not enough readings to upload value for {}".format(wug_type), LogLevel.WARNING,
+                    "Got {} but require at least {}".format(values[1], self._config["min_readings"]))
+            
+        self._log.log("Uploading values to WUG", LogLevel.INFO, str(fields))
+        self._upload_data(fields)
+        
+        self._data.clear()
+
+    def _upload_data(self, data):
+        """ Uploads the specified data fields to WUG
+
+            data: a dict mapping sensor fields (wug_type) to values
+        """
+        if self._stale or len(data) == 0: return
+
+        # Required configuration parameters
+        get_params = {
+            "action": "updateraw",
+            "ID": self._config["pws_id"],
+            "PASSWORD": self._config["pws_key"],
+            "dateutc": datetime.utcnow().isoformat(sep=" ")
+        }
+        # Add the data to the parameters
+        get_params.update(data)
+        success = False
+
+        # Send the request
+        url = "{}?{}".format(self._config["wug_url"], urlencode(get_params))
+        try:
+            response = urlopen(url)
+            body = response.read().decode('utf-8')
+            success = "success" in body
+            if success:
+                self._log.log("Successfully uploaded data to WUG", LogLevel.INFO, body)
+            else:
+                self._log.log("Uploading to WUG was unsuccessful", LogLevel.CRITICAL, body)
+        except URLError as err:
+            self._log.log("Error uploading to WUG", LogLevel.CRITICAL, str(err))
+        
+        # TODO: back up failed uploads
+        if not success:
+            pass
